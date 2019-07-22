44a7d2e86f6b6fad2446670341222d9a443b2de8
Samuel <samueljegrant@outlook.com>
2019-07-22 15:50:57 +1200

    Added areas for App and Web CMS

---
+++ Controllers/AdminPortal/Web/WebDashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deepcove_Trust_Website.Controllers.WebPortal
+{
+    [Authorize]
+    [Area("admin-portal,web")]
+    [Route("/admin-portal/web")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(viewName: "~/Views/AdminPortal/Web/Dashboard.cshtml");
+        }
+    }
+}
