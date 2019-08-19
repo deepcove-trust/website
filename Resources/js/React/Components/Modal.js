a81adc81b6f63c1126f81a981dd797d8851eb6cb
jacksos101 <jacksos101@gmail.com>
2019-08-19 23:29:22 +1200

    Create new React component "Modal"

---
+++ Resources/js/React/Components/Modal.js
@@ -0,0 +1,39 @@
+﻿import React, { Component } from 'react';
+
+export class Modal extends Component {
+
+    getModalSize() {
+
+        switch (this.props.size) {
+            case "large":
+                return "modal-lg";
+            case "small":
+                return "modal-sm"
+            default:
+                return ""
+
+        }
+
+    }
+
+    render() {
+        return (
+            <div id={this.props.id || false} className="modal" tabindex="-1" role="dialog">
+                <div className={`modal-dialog ${this.getModalSize()}`} role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{this.props.title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
