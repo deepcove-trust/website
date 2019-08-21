1b1fa2eb24d7214a9ef2ce6bdb4b607d440aff45
jacksos101 <jacksos101@gmail.com>
2019-08-22 00:07:13 +1200

    Create initial class for EditActionModal
    This will need to be modified significanltly

---
+++ Resources/js/React/CMS-Blocks/Text/TextBlockAction/EditActionModal.js
@@ -0,0 +1,60 @@
+﻿import React, { Component, Fragment } from 'react';
+import { Button } from '../../Components/Button';
+import { Modal } from '../../Components/Modal';
+import { FormGroup, Input } from '../../Components/FormControl';
+import _ from 'lodash';
+import $ from 'jquery';
+
+export default class EditActionModal extends Component {
+
+    constructor(props) {
+        super(props);        
+        this.state = {
+            
+        }
+    }
+
+    cancelEdit() {
+       
+    }
+
+    saveChanges() {
+
+    }
+
+    toggleModal(id, e) {
+        $(`#edit-action-modal`).modal(e ? e : 'toggle');
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Button btnClass="btn btn-warning d-inline" type="button" cb={this.toggleModal.bind(this)}>
+                    {this.props.btnText} <i class="fas fa-cog" />
+                </Button>
+
+                <Modal id={`edit-action-modal`} title="Customize link">
+                    <FormGroup label="Link Text" htmlfor="link-text">
+                        <Input type="text" name="link-text" required />
+                    </FormGroup>
+
+                    <FormGroup label="Link URL " htmlfor="link-url">
+                        <Input type="text" name="link-url" required />
+                    </FormGroup>
+
+                    <div>
+                        <Button btnClass="btn btn-danger" role="button">
+                            Cancel
+                        </Button>
+                        <Button btnClass="btn btn-success float-right" role="button">
+                            Save
+                        </Button>
+                    </div>
+                </Modal>
+            </Fragment>
+        );
+
+    }
+}
+
+// ${this.props.link.id}
