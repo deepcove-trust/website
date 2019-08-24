28e15ca22e0b8b37d62444fd39cc74abf3272301
Samuel <samueljegrant@outlook.com>
2019-08-24 16:20:32 +1200

    Cleaned up C# usings

--- Controllers/Authentication/ResetPasswordController.cs
+++ Controllers/Authentication/ResetPasswordController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Deepcove_Trust_Website.Helpers;
@@ -8,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using Deepcove_Trust_Website.Features.Emails;
 using Deepcove_Trust_Website.Data;
 using Microsoft.Extensions.Logging;
 
