9a0eb76080ca1092d823fd51b8876220fad9d489
jacksos101 <jacksos101@gmail.com>
2019-08-24 14:36:05 +1200

    Alter the behaviour of the 'Add Page' dropdown in quick link settings
    Instead of disabling the dropdown, it will now drop down and show 'All pages have been assigned'.

--- Resources/js/React/AdminPortal/SystemSettings/FooterQuickLinks/AddPageLink.js
+++ Resources/js/React/AdminPortal/SystemSettings/FooterQuickLinks/AddPageLink.js
@@ -18,15 +18,17 @@ export default class SelectPage extends Component {
     render() {
         let disabled = !this.props.pages || this.props.pages && this.props.pages.length == 0;
         let pageLinks;
-        if (this.props.pages) {
+        if (this.props.pages && this.props.pages.length > 0) {            
             pageLinks = this.props.pages.map((page) => {
                 return <a class="dropdown-item" role="presentation" onClick={this.addLink.bind(this, page.id)}>{page.name}</a>
             });
+        } else {
+            pageLinks = <a className="dropdown-item disabled" role="presentation">All pages have been assigned</a>
         }
 
         return (
             <div class="dropdown">
-                <button class="btn btn-info btn-sm float-right dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" disabled={disabled}> Add Page </button>
+                <button class="btn btn-info btn-sm float-right dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button"> Add Page </button>
                 <div class="dropdown-menu" role="menu">
                     {pageLinks}
                 </div>
