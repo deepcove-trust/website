0b4e41799461b75889a23cfe67a74bfe02d9a993
Samuel <samueljegrant@outlook.com>
2019-08-29 23:39:26 +1200

    Boilerplate stuff
    - Included placeholder media block
    - Created second template

---
+++ Resources/js/React/PageTemplates/2.js
@@ -0,0 +1,86 @@
+﻿import React, { Component } from 'react';
+import { render } from 'react-dom';
+import PageMast from '../CMS-Blocks/PageMast';
+import TextBlock from '../CMS-Blocks/Text';
+import Media from '../CMS-Blocks/Media';
+
+import $ from 'jquery';
+
+
+const baseUri = "/api/page"
+
+export default class ReactTemplate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+            pageId: null,
+        }
+    }
+
+    componentDidMount() {
+        if (!document.getElementById('react_template_2')) {
+            throw `Failed to attach component. Attribute 'data-pageid' was not found`;
+        }
+
+        this.setState({
+            pageId: document.getElementById('react_template_2').getAttribute("data-pageid")
+        }, () => {
+            this.getData();
+        });
+    }
+
+    getData() {
+        $.ajax({
+            type: 'get',
+            url: `${baseUri}/${this.state.pageId}`
+        }).done((data) => {
+            this.setState({
+                data: data
+            });
+        }).fail((err) => {
+            console.error(err);
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="row">
+                    <div className="col-12 px-0 mb-4" style={{ 'margin-top': '-1rem' }}>
+                        <Media />
+                    </div>
+
+                    <PageMast page={this.state.data}
+                        baseUri={baseUri}
+                        u={this.getData.bind(this)}
+                    />
+
+                    <div className="col-lg-6 col-md-12 py-2">
+                        <TextBlock
+                            u={this.getData.bind(this)}
+                            baseUri={baseUri}
+                            admin={this.state.data && !!this.state.data.settings}
+                            content={this.state.data ? this.state.data.text[0] : null}
+                            settings={this.state.data ? this.state.data.settings : null}
+                        />
+                    </div>
+
+                    <div className="col-lg-6 col-md-12">
+                        <TextBlock
+                            u={this.getData.bind(this)}
+                            baseUri={baseUri}
+                            admin={this.state.data && !!this.state.data.settings}
+                            content={this.state.data ? this.state.data.text[1] : null}
+                            settings={this.state.data ? this.state.data.settings : null}
+                        />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+if (document.getElementById('react_template_2'))
+    render(<ReactTemplate />, document.getElementById('react_template_2'));    
