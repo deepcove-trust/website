942f740929934543019b40a1310e170a80e4d5c1
Samuel <samueljegrant@outlook.com>
2019-09-04 19:54:23 +1200

    Boiler plate stuff

---
+++ Controllers/AdminPortal/ManageMediaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deepcove_Trust_Website.Controllers.AdminPortal
+{
+    [Authorize]
+    [Area("admin-portal")]
+    [Route("/admin/media")]
+    public class ManageMediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(viewName: "~/Views/AdminPortal/ManageMedia.cshtml");
+        }
+    }
+}
---
+++ Resources/js/React/AdminPortal/ManageMedia.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class ManageMedia extends Component {
+    render() {
+        return <div />
+    }
+}
+
+if (document.getElementById('react_ManageMedia'))
+    render(<ManageMedia />, document.getElementById('react_ManageMedia'));    
