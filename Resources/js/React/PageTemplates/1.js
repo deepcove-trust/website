1d28e7fcfa4869d54681fc459c0655e08fc138c8
Samuel <samueljegrant@outlook.com>
2019-09-08 19:06:22 +1200

    Created Index Template
    ~WIP~ committing as I need to merge feature/media into this branch

--- Resources/js/React/PageTemplates/1.js
+++ Resources/js/React/PageTemplates/1.js
@@ -1,70 +1,41 @@
 ﻿import React, { Component } from 'react';
-import GoogleMap from '../CMS-Blocks/GoogleMap';
 import PageTitle from '../CMS-Blocks/PageTitle';
 import TextBlock from '../CMS-Blocks/Text';
-import EmailForm from '../CMS-Blocks/EmailForm';
-
-
-const baseUri = "/api/page"
+import Media from '../CMS-Blocks/Media';
 
 export default class ReactTemplate1 extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: null,
-            pageId: null,
-        }
-    }
-
-
     render() {
         return (
-            <React.Fragment>
-                <PageTitle title={this.props.data.name}
-                    public={this.props.data.public}
-                    created={this.props.data.created}
-                    displayAdmin={!!this.props.data.enums}
-                />
-
-
-                <div className="row pb-4">
-                    <div className="col-lg-4 col-md-6 col-sm-12">
-                        <TextBlock allowEdits={this.props.allowEdits}
-                            content={this.props.data.textComponents[0] || null}
-                            pushChanges={this.props.pushChanges.bind(this, 'text')}
-                            settings={this.props.data.enums}
-                        />
-                    </div>
-
-                    <div className="col-lg-4 col-md-6 col-sm-12">
-                        <TextBlock allowEdits={this.props.allowEdits}
-                            content={this.props.data.textComponents[1] || null}
-                            pushChanges={this.props.pushChanges.bind(this, 'text')}
-                            settings={this.props.data.enums}
-                        />
-                    </div>
-
-                    <div className="col-lg-4 col-md-6 col-sm-12">
-                        <TextBlock allowEdits={this.props.allowEdits}
-                            content={this.props.data.textComponents[2] || null}
-                            pushChanges={this.props.pushChanges.bind(this, 'text')}
-                            settings={this.props.data.enums}
-                        />
-                    </div>
+            <div className="row">
+                <div className="col-12 px-0">
+                    <Media minHeight="440"/>
                 </div>
 
-                <div className="row pt-2">
-                    <div className="col-lg-6 col-md-12 pb-2">
-                        <EmailForm config={this.props.data.otherComponents.captchaSiteKey}/>
-                    </div>
+                <div className="col-12 pt-3">
+                    <h3>Experience life in a remote part of the Fiordland National Park</h3>
+                </div>
 
-                    <div className="col-lg-6 col-md-12">
-                        <GoogleMap title="Where to Find Us"
-                            config={this.props.data.otherComponents.googleMaps || null } />
+                <div className="col-lg-6 col-md-12">
+                    <TextBlock allowEdits={this.props.allowEdits}
+                        content={this.props.data.textComponents[0] || null}
+                        pushChanges={this.props.pushChanges.bind(this, 'text')}
+                        settings={this.props.data.enums}
+                    />
+                </div>
+                <div className="col-lg-6 col-md-12">
+                    <TextBlock allowEdits={this.props.allowEdits}
+                        content={this.props.data.textComponents[1] || null}
+                        pushChanges={this.props.pushChanges.bind(this, 'text')}
+                        settings={this.props.data.enums}
+                    />
+                </div>
+
+                <div className="row">
+                    <div className="col-lg-3 col-md-2 col-sm-12">
+                        
                     </div>
                 </div>
-            </React.Fragment>
+            </div>
         );
     }
 }
