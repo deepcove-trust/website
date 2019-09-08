5276f5432454d97b257b3b969e25739289d882b2
Samuel <samueljegrant@outlook.com>
2019-09-08 19:22:53 +1200

    Built up Index page template

--- Resources/js/React/PageTemplates/1.js
+++ Resources/js/React/PageTemplates/1.js
@@ -1,41 +1,125 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Component, Fragment } from 'react';
 import PageTitle from '../CMS-Blocks/PageTitle';
 import TextBlock from '../CMS-Blocks/Text';
 import Media from '../CMS-Blocks/Media';
+import Panel from '../Components/Panel';
 
 export default class ReactTemplate1 extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-12 px-0">
-                    <Media minHeight="440"/>
-                </div>
-
-                <div className="col-12 pt-3">
-                    <h3>Experience life in a remote part of the Fiordland National Park</h3>
-                </div>
-
-                <div className="col-lg-6 col-md-12">
-                    <TextBlock allowEdits={this.props.allowEdits}
-                        content={this.props.data.textComponents[0] || null}
-                        pushChanges={this.props.pushChanges.bind(this, 'text')}
-                        settings={this.props.data.enums}
-                    />
-                </div>
-                <div className="col-lg-6 col-md-12">
-                    <TextBlock allowEdits={this.props.allowEdits}
-                        content={this.props.data.textComponents[1] || null}
-                        pushChanges={this.props.pushChanges.bind(this, 'text')}
-                        settings={this.props.data.enums}
-                    />
-                </div>
-
+            <Fragment>
                 <div className="row">
-                    <div className="col-lg-3 col-md-2 col-sm-12">
-                        
+                    <div className="col-12 px-0">
+                        <Media minHeight="440"/>
+                    </div>
+
+                    <div className="col-12 pt-3">
+                        <h3>Experience life in a remote part of the Fiordland National Park</h3>
+                    </div>
+
+                    <div className="col-lg-6 col-md-12">
+                        <TextBlock allowEdits={this.props.allowEdits}
+                            content={this.props.data.textComponents[0] || null}
+                            pushChanges={this.props.pushChanges.bind(this, 'text')}
+                            settings={this.props.data.enums}
+                        />
+                    </div>
+                    <div className="col-lg-6 col-md-12">
+                        <TextBlock allowEdits={this.props.allowEdits}
+                            content={this.props.data.textComponents[1] || null}
+                            pushChanges={this.props.pushChanges.bind(this, 'text')}
+                            settings={this.props.data.enums}
+                        />
                     </div>
                 </div>
-            </div>
+
+
+                <div className="row">
+                    <div className="col-lg-4 col-md-2 col-sm-12 mb-4">
+                        <Panel>
+                            <div style={{ 'margin': '-1rem -1rem 2rem -1rem' }}>
+                                <Media minHeight="200" />
+                            </div>
+
+                            <TextBlock allowEdits={this.props.allowEdits}
+                                content={this.props.data.textComponents[2] || null}
+                                pushChanges={this.props.pushChanges.bind(this, 'text')}
+                                settings={this.props.data.enums}
+                            />
+                        </Panel>
+                    </div>
+
+                    <div className="col-lg-4 col-md-2 col-sm-12 mb-4">
+                        <Panel>
+                            <div style={{ 'margin': '-1rem -1rem 2rem -1rem' }}>
+                                <Media minHeight="200" />
+                            </div>
+
+                            <TextBlock allowEdits={this.props.allowEdits}
+                                content={this.props.data.textComponents[3] || null}
+                                pushChanges={this.props.pushChanges.bind(this, 'text')}
+                                settings={this.props.data.enums}
+                            />
+                        </Panel>
+                    </div>
+
+                    <div className="col-lg-4 col-md-2 col-sm-12 mb-4">
+                        <Panel>
+                            <div style={{ 'margin': '-1rem -1rem 2rem -1rem' }}>
+                                <Media minHeight="200" />
+                            </div>
+
+                            <TextBlock allowEdits={this.props.allowEdits}
+                                content={this.props.data.textComponents[4] || null}
+                                pushChanges={this.props.pushChanges.bind(this, 'text')}
+                                settings={this.props.data.enums}
+                            />
+                        </Panel>
+                    </div>
+
+                    <div className="col-lg-4 col-md-2 col-sm-12 mb-4">
+                        <Panel>
+                            <div style={{ 'margin': '-1rem -1rem 2rem -1rem' }}>
+                                <Media minHeight="200" />
+                            </div>
+
+                            <TextBlock allowEdits={this.props.allowEdits}
+                                content={this.props.data.textComponents[5] || null}
+                                pushChanges={this.props.pushChanges.bind(this, 'text')}
+                                settings={this.props.data.enums}
+                            />
+                        </Panel>
+                    </div>
+
+                    <div className="col-lg-4 col-md-2 col-sm-12 mb-4">
+                        <Panel>
+                            <div style={{ 'margin': '-1rem -1rem 2rem -1rem' }}>
+                                <Media minHeight="200" />
+                            </div>
+
+                            <TextBlock allowEdits={this.props.allowEdits}
+                                content={this.props.data.textComponents[6] || null}
+                                pushChanges={this.props.pushChanges.bind(this, 'text')}
+                                settings={this.props.data.enums}
+                            />
+                        </Panel>
+                    </div>
+
+                    <div className="col-lg-4 col-md-2 col-sm-12 mb-4">
+                        <Panel>
+                            <div style={{ 'margin': '-1rem -1rem 2rem -1rem' }}>
+                                <Media minHeight="200" />
+                            </div>
+
+                            <TextBlock allowEdits={this.props.allowEdits}
+                                content={this.props.data.textComponents[7] || null}
+                                pushChanges={this.props.pushChanges.bind(this, 'text')}
+                                settings={this.props.data.enums}
+                            />
+                        </Panel>
+                    </div>
+                </div>
+            </Fragment>
         );
     }
 }
