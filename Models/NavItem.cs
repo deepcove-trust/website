79b215215ada48eed5073dca4994bb8371299614
Samuel <samueljegrant@outlook.com>
2019-10-30 13:50:18 +1300

    Custom nav link text support

--- Models/NavItem.cs
+++ Models/NavItem.cs
@@ -7,7 +7,8 @@ namespace Deepcove_Trust_Website.Models
         public int Id { get; set; }
         public Section Section { get; set; }
         public int? PageId { get; set; }
-
+        public string Text { get; set; }
+            
         // Navigation properties
         public Page Page { get; set; }
         public List<NavItemPage> NavItemPages { get; set; }
