1f80f39370fc0e50cafdb1579d323bd1fba87050
jacksos101 <jacksos101@gmail.com>
2019-12-22 10:30:09 +1300

    Cascade page deletes to remove nav items and sublinks from database

--- Data/WebsiteDataContext.cs
+++ Data/WebsiteDataContext.cs
@@ -178,7 +178,8 @@ namespace Deepcove_Trust_Website.Data
             modelBuilder.Entity<FactFileEntry>().HasMany(ff => ff.FactFileEntryImages).WithOne(ei => ei.FactFileEntry).HasForeignKey(ei => ei.FactFileEntryId).OnDelete(DeleteBehavior.Restrict);
 
             // -- Configure relationships between pages and navitems
-            modelBuilder.Entity<Page>().HasMany(p => p.NavItems).WithOne(ni => ni.Page).HasForeignKey(ni => ni.PageId).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Page>().HasMany(p => p.NavItems).WithOne(ni => ni.Page).HasForeignKey(ni => ni.PageId).OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Page>().HasMany(p => p.NavItemPages).WithOne(ni => ni.Page).HasForeignKey(ni => ni.PageId).OnDelete(DeleteBehavior.Cascade);
         }
 
         /// <summary>
--- Migrations/20191221212531_cascade-page-delete.Designer.cs
+++ Migrations/20191221212531_cascade-page-delete.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Deepcove_Trust_Website.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Deepcove_Trust_Website.Migrations
 {
     [DbContext(typeof(WebsiteDataContext))]
-    partial class WebsiteDataContextModelSnapshot : ModelSnapshot
+    [Migration("20191221212531_cascade-page-delete")]
+    partial class cascadepagedelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -390,6 +392,8 @@ namespace Deepcove_Trust_Website.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
+                    b.Property<int>("OrderIndex");
+
                     b.Property<int?>("PageId");
 
                     b.Property<int>("Section");
@@ -411,6 +415,8 @@ namespace Deepcove_Trust_Website.Migrations
                         .ValueGeneratedOnAdd();
 
                     b.Property<int>("NavItemId");
+
+                    b.Property<int>("OrderIndex");
 
                     b.Property<int?>("PageId");
 
