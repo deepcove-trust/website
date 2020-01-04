8a3a7c65f1f601421efb3aeb646fe4bf89dff266
Samuel Grant <samueljegrant@outlook.com>
2020-01-04 15:10:48 +1300

    Added a Phone CMS preview React Class
    This can be called using
    ```cs
    <PhonePreview>
    
    </PhonePreview>
    ```

---
+++ Resources/js/React/Components/PhonePreview.js
@@ -0,0 +1,9 @@
+﻿import React, { Component, Fragment } from 'react';
+
+export default class PhonePreview extends Component {
+    render() {
+        <div>
+            {this.props.children}
+        </div>
+    }
+}
