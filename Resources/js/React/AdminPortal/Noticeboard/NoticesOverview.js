effee9a23ef94af528855d9bc46a3ec453e76a89
samueljegrant <samueljegrant@outlook.com>
2020-01-16 15:07:35 +1300

    ~WIP~ Notices list

--- Resources/js/React/AdminPortal/Noticeboard/NoticesOverview.js
+++ Resources/js/React/AdminPortal/Noticeboard/NoticesOverview.js
@@ -5,7 +5,7 @@ import Notice from './Notice';
 
 export default class NoticesOverview extends Component {
     render() {
-        const { important, normal } = this.props;
+        const { important, normal, disabled } = this.props;
 
         let important_notices = important.map((notice, key) => {
             return <Notice notice={notice} key={key} important />
@@ -15,12 +15,24 @@ export default class NoticesOverview extends Component {
             return <Notice notice={notice} key={key} />
         });
 
+        let notice_list = important.map((notice, key) => {
+            return notice.title;
+        });
+
+        notice_list += normal.map((notice, key) => {
+            return notice.title;
+        });
+
+        notice_list += disabled.map((notice, key) => {
+            return notice.title;
+        });
+
 
         return (
             <Fragment>
                 <div className="col-md-8 col-sm-12">
                     <div className="row">
-   
+                        {notice_list}
                     </div>
                 </div>
 
