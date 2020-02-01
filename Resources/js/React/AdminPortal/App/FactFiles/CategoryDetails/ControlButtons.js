ddcd2d45f226cd37e0c71b43911e2148e6ea7a1f
jacksos101 <jacksos101@gmail.com>
2020-02-01 21:10:34 +1300

    Fix overflowing media select modal, fix cancel button when adding new entry

--- Resources/js/React/AdminPortal/App/FactFiles/CategoryDetails/ControlButtons.js
+++ Resources/js/React/AdminPortal/App/FactFiles/CategoryDetails/ControlButtons.js
@@ -5,7 +5,7 @@ export default class ControlButtons extends Component {
     render() {
         return (
             <div className={`text-center control-buttons ${this.props.show ? "control-buttons-show" : ""}`}>
-                <ConfirmButton className="btn btn-danger" cb={this.props.onDiscard.bind(this)}>{this.props.addEntryMode ? "Cancel" : "Discard Changes"}</ConfirmButton>
+                <ConfirmButton className="btn btn-danger" cb={this.props.addEntryMode ? this.props.onCancelNew.bind(this) : this.props.onDiscard.bind(this)}>{this.props.addEntryMode ? "Cancel" : "Discard Changes"}</ConfirmButton>
                 <ConfirmButton className="btn btn-success m-1 mr-3" cb={this.props.onSave.bind(this)}>{this.props.addEntryMode ? "Save Entry": "Save Changes"}</ConfirmButton>
             </div>
         )
